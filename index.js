75668f0ffaed2eabc0c079d5a7dd447268fe78a9
Eric O'Connell <eric.oconnell@idealist.org>
2015-07-06 14:56:40 -0700

    Catch errors and give informative messages

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ var jsxlate = require('jsxlate');
 
 module.exports = function(src) {
     this.cacheable();
-    return jsxlate.translator.transformMessageNodes(src);
+    try {
+        return jsxlate.translator.transformMessageNodes(src);
+    } catch(e) {
+        var message = "Error transforming " + this.resource;
+        console.error(message);
+        console.error(e.toString());
+        var originalMessage = e.toString();
+        e.toString = function() { return message + " (Original exception: " + originalMessage + ")"; };
+        throw e;
+    }
 };
